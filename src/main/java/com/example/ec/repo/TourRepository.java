968451353d9765d2c9114bcdd9982fc0243bb1de
Samyak Padwekar <samyakpadwekar.23@gmail.com>
2024-03-28 18:25:28 +0530

    Paging And Sorting

--- src/main/java/com/example/ec/repo/TourRepository.java
+++ src/main/java/com/example/ec/repo/TourRepository.java
@@ -4,7 +4,11 @@ import com.example.ec.domain.Tour;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
 
@@ -13,13 +17,14 @@ import org.springframework.data.repository.query.Param;
  *
  * Created by Mary Ellen Bowman
  */
-public interface TourRepository extends CrudRepository<Tour, Integer> {
-	
+//public interface TourRepository extends PagingAndSortingRepository<Tour, Integer>,CrudRepository<Tour, Integer> {
+public interface TourRepository extends JpaRepository<Tour, Integer> {	
 	/**
      * Find Tours associated with the Tour Package.
      *
      * @param code tour package code
      * @return List of found tours.
      */
-    List<Tour> findByTourPackageCode(@Param("code")String code);
+	//added paging
+    Page<Tour> findByTourPackageCode(@Param("code")String code,Pageable pageable);
 }
