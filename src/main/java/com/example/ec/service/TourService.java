bd64842b03a0d4cfa1fdef42df2d884330726c57
Samyak Padwekar <samyakpadwekar.23@gmail.com>
2024-03-29 16:28:24 +0530

    added verifyTour method

--- src/main/java/com/example/ec/service/TourService.java
+++ src/main/java/com/example/ec/service/TourService.java
@@ -4,61 +4,73 @@ import com.example.ec.domain.Difficulty;
 import com.example.ec.domain.Region;
 import com.example.ec.domain.Tour;
 import com.example.ec.domain.TourPackage;
+import com.example.ec.exception.NoSuchElementException;
 import com.example.ec.repo.TourPackageRepository;
 import com.example.ec.repo.TourRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
- * Tour  Service
+ * Tour Service
  *
  * Created by Mary Ellen Bowman
  */
 
 @Service
 public class TourService {
-    private TourRepository tourRepository;
-    private TourPackageRepository tourPackageRepository;
+	private TourRepository tourRepository;
+	private TourPackageRepository tourPackageRepository;
 
-    @Autowired
-    public TourService(TourRepository tourRepository, TourPackageRepository tourPackageRepository) {
-        this.tourRepository = tourRepository;
-        this.tourPackageRepository = tourPackageRepository;
-    }
+	@Autowired
+	public TourService(TourRepository tourRepository, TourPackageRepository tourPackageRepository) {
+		this.tourRepository = tourRepository;
+		this.tourPackageRepository = tourPackageRepository;
+	}
 
-    /**
-     * Create a new Tour Object and persist it to the Database.
-     *
-     * @param title
-     * @param description
-     * @param blurb
-     * @param price
-     * @param duration
-     * @param bullets
-     * @param keywords
-     * @param tourPackageName
-     * @param difficulty
-     * @param region
-     * @return Tour Entity
-     */
-    public Tour createTour(String title, String description, String blurb, Integer price,
-                           String duration, String bullets,
-                           String keywords, String tourPackageName, Difficulty difficulty, Region region ) {
-        TourPackage tourPackage = tourPackageRepository.findByName(tourPackageName).orElseThrow(()->
-             new RuntimeException("Tour package does not exist: " + tourPackageName));
+	/**
+	 * Create a new Tour Object and persist it to the Database.
+	 *
+	 * @param title
+	 * @param description
+	 * @param blurb
+	 * @param price
+	 * @param duration
+	 * @param bullets
+	 * @param keywords
+	 * @param tourPackageName
+	 * @param difficulty
+	 * @param region
+	 * @return Tour Entity
+	 */
+	public Tour createTour(String title, String description, String blurb, Integer price, String duration,
+			String bullets, String keywords, String tourPackageName, Difficulty difficulty, Region region) {
+		TourPackage tourPackage = tourPackageRepository.findByName(tourPackageName)
+				.orElseThrow(() -> new RuntimeException("Tour package does not exist: " + tourPackageName));
 
-        return tourRepository.save(new Tour(title, description,blurb, price, duration,
-                bullets, keywords, tourPackage, difficulty, region));
-    }
+		return tourRepository.save(new Tour(title, description, blurb, price, duration, bullets, keywords, tourPackage,
+				difficulty, region));
+	}
 
-    /**
-     * Calculate the number of Tours in the Database.
-     *
-     * @return the total.
-     */
-    public long total() {
-        return tourRepository.count();
-    }
+	/**
+	 * Verify and return the Tour given a tourId.
+	 *
+	 * @param tourId tour identifier
+	 * @return the found Tour
+	 * @throws NoSuchElementException if no Tour found.
+	 */
+	public Tour verifyTour(int tourId) throws NoSuchElementException {
+		return tourRepository.findById(tourId)
+				.orElseThrow(() -> new NoSuchElementException("Tour does not exist " + tourId));
+	}
+
+	/**
+	 * Calculate the number of Tours in the Database.
+	 *
+	 * @return the total.
+	 */
+	public long total() {
+		return tourRepository.count();
+	}
 
 }
-
