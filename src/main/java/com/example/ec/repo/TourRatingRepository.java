5b270257d6888d382f9547953b1bbe40ae722cc7
Samyak Padwekar <samyakpadwekar.23@gmail.com>
2024-03-29 14:26:52 +0530

    TourRating repo

---
+++ src/main/java/com/example/ec/repo/TourRatingRepository.java
@@ -0,0 +1,33 @@
+package com.example.ec.repo;
+
+import com.example.ec.domain.TourRating;
+import com.example.ec.domain.TourRatingPk;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Tour Rating Repository Interface
+ */
+//to prevent this repo from being exposed to spring data rest
+@RepositoryRestResource(exported = false)
+public interface TourRatingRepository extends CrudRepository<TourRating, TourRatingPk> {
+
+    /**
+     * Lookup all the TourRatings for a tour.
+     *
+     * @param tourId is the tour Identifier
+     * @return a List of any found TourRatings
+     */
+    List<TourRating> findByPkTourId(Integer tourId);
+
+    /**
+     * Lookup a TourRating by the TourId and Customer Id
+     * @param tourId tour identifier
+     * @param customerId customer identifier
+     * @return Optional of found TourRatings.
+     */
+    Optional<TourRating> findByPkTourIdAndPkCustomerId(Integer tourId, Integer customerId);
+}
