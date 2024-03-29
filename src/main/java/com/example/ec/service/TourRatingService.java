e5dde28d3dd480f24beb50dc6c2939afc6ca71f3
Samyak Padwekar <samyakpadwekar.23@gmail.com>
2024-03-29 16:28:54 +0530

    added method to verify TourRating object

---
+++ src/main/java/com/example/ec/service/TourRatingService.java
@@ -0,0 +1,33 @@
+package com.example.ec.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.ec.domain.TourRating;
+import com.example.ec.exception.NoSuchElementException;
+import com.example.ec.repo.TourRatingRepository;
+
+@Service
+public class TourRatingService {
+	private TourRatingRepository tourRatingRepository;
+
+	@Autowired
+	public TourRatingService(TourRatingRepository tourRatingRepository) {
+		this.tourRatingRepository = tourRatingRepository;
+	}
+
+	/**
+	 * Verify and return the TourRating for a particular tourId and Customer
+	 * 
+	 * @param tourId     tour identifier
+	 * @param customerId customer identifier
+	 * @return the found TourRating
+	 * @throws NoSuchElementException if no TourRating found
+	 */
+	public TourRating verifyTourRating(int tourId, int customerId) throws NoSuchElementException {
+		return tourRatingRepository.findByPkTourIdAndPkCustomerId(tourId, customerId)
+				.orElseThrow(() -> new NoSuchElementException(
+						"Tour-Rating pair for request(" + tourId + " for customer" + customerId));
+	}
+
+}
