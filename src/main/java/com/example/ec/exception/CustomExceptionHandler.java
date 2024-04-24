90ca31ad54033aab70ef235c84ee4d996bce1f10
Samyak Padwekar <samyakpadwekar.23@gmail.com>
2024-04-25 01:36:53 +0530

    customException handler

---
+++ src/main/java/com/example/ec/exception/CustomExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.example.ec.exception;
+
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpServerErrorException;
+
+import io.jsonwebtoken.ExpiredJwtException;
+
+/**
+ * Handle all Exceptions in for All controllers
+ */
+@RestControllerAdvice
+public class CustomExceptionHandler {
+	
+	@ExceptionHandler(Exception.class)
+	public ProblemDetail handleSecurityException(Exception ex) {
+        ProblemDetail errorDetail = null;
+        if (ex instanceof BadCredentialsException) {
+            errorDetail = ProblemDetail
+                    .forStatusAndDetail(HttpStatusCode.valueOf(401), ex.getMessage());
+            errorDetail.setProperty("access_denied_reason", "Authentication Failure");
+        }
+
+        if (ex instanceof AccessDeniedException) {
+            errorDetail = ProblemDetail
+                    .forStatusAndDetail(HttpStatusCode.valueOf(403), ex.getMessage());
+            errorDetail.setProperty("access_denied_reason", "not_authorized!");
+
+        }
+
+        if (ex instanceof HttpServerErrorException) {
+            errorDetail = ProblemDetail
+                    .forStatusAndDetail(HttpStatusCode.valueOf(403), ex.getMessage());
+            errorDetail.setProperty("access_denied_reason", "Login failed for user");
+        }
+        if (ex instanceof ExpiredJwtException) {
+            errorDetail = ProblemDetail
+                    .forStatusAndDetail(HttpStatusCode.valueOf(403), ex.getMessage());
+            errorDetail.setProperty("access_denied_reason", "JWT Token already expired !");
+        }
+
+        return errorDetail;
+    }
+}
+
